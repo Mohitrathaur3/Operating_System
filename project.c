8c810628e3b55956f3a7df326f90e01e5aed4ccd
Mohit Rathaur <mohitrathaur3@gmail.com>
2018-03-28 09:52:29 +0530

    commit_2

--- project.c
+++ project.c
@@ -23,4 +23,55 @@ void Average_Time(int process[], int n, int burst[],int quant,char name)
 	}
         printf("%d \t\t  %d \t\t  %d \t \t %d \t\t\t%d\n",i,process[i],burst[i],wt[i],temp[i]);
     }
+// average query time and average waiting time
+    avg_wait=total_wt/(i-1);
+    avg_turnaround=total_time/(i-1);
+    if(name=='s')
+    {
+	printf("\n Time he spend on handling of students query is : %d",avg_turnaround);
+        printf("\n Average query time is : %d",avg_wait);
+    }
+    else
+    {
+	printf("\n Time he spend on handling of faculty query is : %d",avg_turnaround);
+        printf("\n Average query time is : %d",avg_wait);
+    }
+}
 
+//waiting time frome here
+
+void WaitingTime(int process[], int n,int burst[], int wt[], int quan)
+{
+    int i,t,rem[n];
+    t=0;
+    for (i = 1 ; i <= n ; i++)
+        {
+            rem[i] =  burst[i];
+        }
+    while(1)
+    {
+        bool flag= true;
+        for(i = 1; i <= n; i++)
+        {
+            if (rem[i]>0)
+            {
+                flag=false; 
+ 
+                if (rem[i]>quan)
+                {
+                    t += quan;
+                    rem[i] -= quan;
+                }
+                else
+                {
+	            wt[i] = t - process[i];
+                    t = t + rem[i];
+                    //wt[i] = t - burst[i];
+                    rem[i] = 0;
+                }
+            }
+        }
+        if (flag == true)
+          break;
+    }
+}
